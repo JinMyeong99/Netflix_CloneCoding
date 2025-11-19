4a2551f86c98c8ec03b73db0630ea9725b4fcaed
JinMyeong Choi <ymo6538@gmail.com>
2025-11-19 14:06:04 +0900

    thunk 삼항연산자 수정

--- src/api/tmdb.js
+++ src/api/tmdb.js
@@ -3,7 +3,4 @@ export const ApiKey = import.meta.env.VITE_TMDB_API_KEY;
 export const BaseUrl = "https://api.themoviedb.org/3";
 export const BaseImageUrl = "https://image.tmdb.org/t/p/original";
 
-export const ImageUrl = (path) => {
-  if (!path) return "";
-  return `${BaseImageUrl}${path}`;
-};
+export const ImageUrl = (path) => (!path ? "" : `${BaseImageUrl}${path}`);
