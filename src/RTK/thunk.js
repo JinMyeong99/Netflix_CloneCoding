dba8adb23ca5bf2d4f4276ea7b415c3c52c3ad04
JinMyeong Choi <ymo6538@gmail.com>
2025-11-19 13:10:02 +0900

    thunk 추가

---
+++ src/RTK/thunk.js
@@ -0,0 +1,35 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { ApiKey, BaseUrl } from "../api/tmdb";
+
+export const fetchMovieData = createAsyncThunk(
+  "movie/fetchMovieData",
+  async () => {
+    const popularUrl = `${BaseUrl}/movie/popular?api_key=${ApiKey}&language=ko-KR&page=1`;
+    const topRatedUrl = `${BaseUrl}/movie/top_rated?api_key=${ApiKey}&language=ko-KR&page=1`;
+    const upcomingUrl = `${BaseUrl}/movie/upcoming?api_key=${ApiKey}&language=ko-KR&page=1`;
+    const trendingUrl = `${BaseUrl}/movie/trending?api_key=${ApiKey}&language=ko-KR`;
+
+    const [popularRes, topRatedRes, upcomingRes, trendingRes] =
+      await Promise.all([
+        fetch(popularUrl),
+        fetch(topRatedUrl),
+        fetch(upcomingUrl),
+        fetch(trendingUrl),
+      ]);
+
+    const [popularData, topRatedData, upcomingData, trendingData] =
+      await Promise.all([
+        popularRes.json(),
+        topRatedRes.json(),
+        upcomingRes.json(),
+        trendingRes.json(),
+      ]);
+
+    return {
+      popularData: popularData.results,
+      topRatedData: topRatedData.results,
+      upcomingData: upcomingData.results,
+      trendingData: trendingData.results,
+    };
+  }
+);
