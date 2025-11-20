b5e0845d060df124c5384637fb5ed62d3e7d767b
JinMyeong Choi <ymo6538@gmail.com>
2025-11-20 11:44:18 +0900

    인기 영화 무한 리스트 추가

--- src/RTK/movieThunk.js
+++ src/RTK/movieThunk.js
@@ -33,3 +33,27 @@ export const fetchMovieData = createAsyncThunk(
     };
   }
 );
+
+export const fetchPopularMoviesPage = createAsyncThunk(
+  "movies/fetchPopularMoviesPage",
+  async (_, { getState, rejectWithValue }) => {
+    const { movies } = getState();
+    const { page, morePage } = movies;
+
+    if (!morePage) {
+      return rejectWithValue;
+    }
+
+    const nextPage = page + 1;
+
+    const pageUrl = `${BaseUrl}/movie/popular?api_key=${ApiKey}&language=ko-KR&page=${nextPage}`;
+    const res = await fetch(pageUrl);
+    const moreMovies = await res.json();
+
+    return {
+      page: nextPage,
+      results: moreMovies.results,
+      totalPages: moreMovies.total_pages,
+    };
+  }
+);
