f997ea3a0170eb6398b037a3d9a06c4e4dbb32bb
JinMyeong Choi <ymo6538@gmail.com>
2025-11-21 11:08:05 +0900

    movie폴더 이름 바꾸기

--- src/RTK/store.js
+++ src/RTK/store.js
@@ -1,12 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { homeSlice } from "./home/homeSlice";
-import { popularMoviesSlice } from "./moviePopular/popularSlice";
+import { MovieSlice } from "./moviePopular/movieSlice";
 import { tvSlice } from "./tv/tvSlice";
 
 export const store = configureStore({
   reducer: {
     home: homeSlice.reducer,
-    popularMovies: popularMoviesSlice.reducer,
+    movie: MovieSlice.reducer,
     tv: tvSlice.reducer,
   },
 });
