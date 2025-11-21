2ea23e38c30bfbae496c4e2f04d575e5bba4c02a
JinMyeong Choi <ymo6538@gmail.com>
2025-11-22 00:26:13 +0900

    에러 추가

--- src/RTK/home/homeThunk.js
+++ src/RTK/home/homeThunk.js
@@ -3,34 +3,41 @@ import { ApiKey, BaseUrl } from "../../api/tmdb";
 
 export const fetchHomeData = createAsyncThunk(
   "home/fetchHomeData",
-  async () => {
-    const popularUrl = `${BaseUrl}/movie/popular?api_key=${ApiKey}&language=ko-KR&page=1`;
-    const topRatedUrl = `${BaseUrl}/movie/top_rated?api_key=${ApiKey}&language=ko-KR&page=1`;
-    const upcomingUrl = `${BaseUrl}/movie/upcoming?api_key=${ApiKey}&language=ko-KR&page=1`;
-    const trendingUrl = `${BaseUrl}/trending/movie/week?api_key=${ApiKey}&language=ko-KR`;
+  async (_, { rejectWithValue }) => {
+    try {
+      const popularUrl = `${BaseUrl}/movie/popular?api_key=${ApiKey}&language=ko-KR&page=1`;
+      const topRatedUrl = `${BaseUrl}/movie/top_rated?api_key=${ApiKey}&language=ko-KR&page=1`;
+      const upcomingUrl = `${BaseUrl}/movie/upcoming?api_key=${ApiKey}&language=ko-KR&page=1`;
+      const trendingUrl = `${BaseUrl}/trending/movie/week?api_key=${ApiKey}&language=ko-KR`;
 
-    const [popularRes, topRatedRes, upcomingRes, trendingRes] =
-      await Promise.all([
-        fetch(popularUrl),
-        fetch(topRatedUrl),
-        fetch(upcomingUrl),
-        fetch(trendingUrl),
-      ]);
+      const [popularRes, topRatedRes, upcomingRes, trendingRes] =
+        await Promise.all([
+          fetch(popularUrl),
+          fetch(topRatedUrl),
+          fetch(upcomingUrl),
+          fetch(trendingUrl),
+        ]);
 
-    const [popularData, topRatedData, upcomingData, trendingData] =
-      await Promise.all([
-        popularRes.json(),
-        topRatedRes.json(),
-        upcomingRes.json(),
-        trendingRes.json(),
-      ]);
+        if(!popularRes.ok || !topRatedRes.ok || !upcomingRes.ok || !trendingRes.ok){
+          throw new Error("홈 데이터 로딩 실패")
+        }
 
-    return {
-      popular: popularData.results,
-      topRated: topRatedData.results,
-      upcoming: upcomingData.results,
-      trending: trendingData.results,
-    };
+      const [popularData, topRatedData, upcomingData, trendingData] =
+        await Promise.all([
+          popularRes.json(),
+          topRatedRes.json(),
+          upcomingRes.json(),
+          trendingRes.json(),
+        ]);
+
+      return {
+        popular: popularData.results,
+        topRated: topRatedData.results,
+        upcoming: upcomingData.results,
+        trending: trendingData.results,
+      };
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
   }
 );
-
