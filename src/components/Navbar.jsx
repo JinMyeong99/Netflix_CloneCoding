ff52003ea0673128760a1364e34f30b85b4577b9
JinMyeong Choi <ymo6538@gmail.com>
2025-11-21 15:47:37 +0900

    img에 link추가

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,16 +3,19 @@ import Netflix_logo from "../assets/Netflix_logo.png";
 
 export default function Navbar() {
   return (
-    <header>
-      <img className="h-[26px]" src={Netflix_logo} />
-      <nav>
+    <header className="flex justify-between">
+      <nav className="flex">
+        <Link to="/">
+          <img className="flex h-[26px]" src={Netflix_logo} />
+        </Link>
+
         <Link to="/">홈</Link>
         <Link to="/series">시리즈</Link>
         <Link to="/movie">영화</Link>
         <Link to="/trending">트렌드</Link>
         <Link to="/favorite">내가 찜한 리스트</Link>
       </nav>
-      <div>검색/로그인</div>
+      <div className="flex">검색/로그인</div>
     </header>
   );
 }
