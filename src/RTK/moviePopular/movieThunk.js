5859282fef7379fb15293db72d7ca022314b7d7e
JinMyeong Choi <ymo6538@gmail.com>
2025-11-22 18:08:49 +0900

    movieThunk 에러추가

--- src/RTK/moviePopular/movieThunk.js
+++ src/RTK/moviePopular/movieThunk.js
@@ -4,23 +4,30 @@ import { ApiKey, BaseUrl } from "../../api/tmdb";
 export const fetchMoviePage = createAsyncThunk(
   "movie/fetchMoviePage",
   async (_, { getState, rejectWithValue }) => {
-    const { movie } = getState();
-    const { page, morePage } = movie;
+    try {
+      const { movieState } = getState().movie;
+      const { page, hasMore, loading } = movieState;
 
-    if (!morePage) {
-      return rejectWithValue;
+      if (!hasMore) {
+        return rejectWithValue("가져올 페이지가 없습니다");
+      }
+      if (loading) {
+        return rejectWithValue("이미 로딩 중...");
+      }
+
+      const nextPage = page + 1;
+
+      const pageUrl = `${BaseUrl}/movie/popular?api_key=${ApiKey}&language=ko-KR&page=${nextPage}`;
+      const res = await fetch(pageUrl);
+      const moreMovie = await res.json();
+
+      return {
+        page: nextPage,
+        results: moreMovie.results,
+        totalPages: moreMovie.total_pages,
+      };
+    } catch (error) {
+      return rejectWithValue(error.message);
     }
-
-    const nextPage = page + 1;
-
-    const pageUrl = `${BaseUrl}/movie/popular?api_key=${ApiKey}&language=ko-KR&page=${nextPage}`;
-    const res = await fetch(pageUrl);
-    const moreMovie = await res.json();
-
-    return {
-      page: nextPage,
-      results: moreMovie.results,
-      totalPages: moreMovie.total_pages,
-    };
   }
 );
