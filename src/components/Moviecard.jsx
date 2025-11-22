ec2f3e5a7d993daae27d6fbf6dd30825ead47ef8
JinMyeong Choi <ymo6538@gmail.com>
2025-11-22 12:04:29 +0900

    moviecard 기본버전

--- src/components/Moviecard.jsx
+++ src/components/Moviecard.jsx
@@ -1,3 +1,18 @@
-export default function MovieCard() {
-  return <></>;
+import { ImageUrl } from "../api/tmdb";
+
+export default function MovieCard({ movie }) {
+  const title = movie.title || movie.name;
+  const poster = ImageUrl(movie.poster_path || movie.backdrop_path, "w500");
+  const year =
+    movie.relase_date?.slice(0, 4) || movie.first_air_date?.slice(0, 4) || "";
+
+  return (
+    <article>
+      <div>{poster ? <img src={poster} /> : <div>NO IMAGE</div>}</div>
+      <div>
+        <div>{title}</div>
+        {year && <div>{year}</div>}
+      </div>
+    </article>
+  );
 }
