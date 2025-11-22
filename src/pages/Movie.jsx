53d8790f17d3f476f6918c60af5b0abe49507626
JinMyeong Choi <ymo6538@gmail.com>
2025-11-22 20:17:41 +0900

    movie page 저장

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,3 +1,28 @@
+import { useCallback, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { MovieSlice } from "../RTK/moviePopular/movieSlice";
+import { fetchMoviePage } from "../RTK/moviePopular/movieThunk";
+
 export default function Movie() {
+  const dispatch = useDispatch();
+  const { list, loading, hasMore, page, error } = useSelector((state) => {
+    state.movie;
+
+    useEffect(() => {
+      if (page === 0 && list.length === 0) {
+        dispatch(MovieSlice.actions.resetMovie());
+        dispatch(fetchMoviePage());
+      }
+    }, [dispatch, page, list.length]);
+
+    const loadMore = useCallback(() => {
+      if (!loading && hasMore) {
+        dispatch(fetchMoviePage());
+      }
+    }, [dispatch, loading, hasMore]);
+
+    const loaderRef = 
+  });
+
   return <></>;
 }
