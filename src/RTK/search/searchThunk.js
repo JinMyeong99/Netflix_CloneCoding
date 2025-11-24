4b45300a17674a6825b2d91c3957f0d8598f9a47
JinMyeong Choi <ymo6538@gmail.com>
2025-11-24 15:41:03 +0900

    search RTK 완료

---
+++ src/RTK/search/searchThunk.js
@@ -0,0 +1,42 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { ApiKey, BaseUrl } from "../../api/tmdb";
+
+export const fetchSearchPage = createAsyncThunk(
+  "search/fetchSearchPage",
+  async (_, { getState, rejectWithValue }) => {
+    try {
+      const { query, page, hasMore, loading } = getState().search;
+
+      const searchValue = query.trim();
+      if (!searchValue) {
+        return rejectWithValue("검색어 없음");
+      }
+      if (!hasMore) {
+        return rejectWithValue("더 이상 검색 결과가 존재하지 않습니다.");
+      }
+      if (loading) {
+        return rejectWithValue("이미 로딩 중...");
+      }
+
+      const nextPage = page + 1;
+
+      const searchUrl = `${BaseUrl}/search/multi?api_key=${ApiKey}&language=ko-KR&include_adult=false&query=${encodeURIComponent(searchValue)}&page=${nextPage}`;
+
+      const res = await fetch(searchUrl);
+      if (!res.ok) throw new Error("검색 결과 로딩 실패");
+      const searchData = await res.json();
+
+      const filteredData = (searchData.results || []).filter(
+        (item) => item.media_type === "movie" || item.media_type === "tv"
+      );
+
+      return {
+        page: nextPage,
+        results: filteredData,
+        totalPages: filteredData.totoal_pages || nextPage,
+      };
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
--- src/RTK/store.js
+++ src/RTK/store.js
@@ -3,6 +3,7 @@ import { homeSlice } from "./home/homeSlice";
 import { MovieSlice } from "./movie/movieSlice";
 import { seriesSlice } from "./series/seriesSlice";
 import { trendingSlice } from "./trending/trendingSlice";
+import { searchSlice } from "./search/searchSlice";
 
 export const store = configureStore({
   reducer: {
@@ -10,5 +11,6 @@ export const store = configureStore({
     movie: MovieSlice.reducer,
     series: seriesSlice.reducer,
     trending: trendingSlice.reducer,
+    search: searchSlice.reducer,
   },
 });
