3a42be6909aa60f4a650a9484a5b8767c9c38e88
JinMyeong Choi <ymo6538@gmail.com>
2025-11-24 13:58:52 +0900

    trendSlice 완료

--- src/RTK/trend/trendSlice.js
+++ src/RTK/trend/trendSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { fetchTrendPage } from "./trendThunk";
 
 export const trendSlice = createSlice({
   name: "trend",
@@ -20,6 +21,26 @@ export const trendSlice = createSlice({
   },
   extraReducers: (builder) => {
     builder
-      .addCase()
-  }
+      .addCase(fetchTrendPage.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchTrendPage.rejected, (state, action) => {
+        state.loading = false;
+        if (action.payload === "더 이상 페이지가 없습니다.") return;
+
+        state.error = action.payload || action.error.message;
+      })
+      .addCase(fetchTrendPage.fulfilled, (state, action) => {
+        state.loading = false;
+        const { page, results, totalPages } = action.payload;
+
+        state.page = page;
+        state.list.push(...results);
+
+        if (page >= totalPages || results.length === 0) {
+          state.hasMore = false;
+        }
+      });
+  },
 });
