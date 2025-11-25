ce877e98137cdb37601da7bbf3f7c7e634339998
JinMyeong Choi <ymo6538@gmail.com>
2025-11-25 20:15:34 +0900

    tailwind스크롤바 설정

--- src/components/SectionRow.jsx
+++ src/components/SectionRow.jsx
@@ -8,7 +8,7 @@ export default function SectionRow({ title, items }) {
   return (
     <section>
       <h2>{title}</h2>
-      <div className="flex gap-2 overflow-x-auto py-2 ">
+      <div className="flex gap-2 overflow-x-auto py-2">
         {items.map((movie) => (
           <div key={movie.id}>
             <MovieCard movie={movie} />
