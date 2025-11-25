39e187dd17271dda9588ec911e34d462eea45343
JinMyeong Choi <ymo6538@gmail.com>
2025-11-25 16:39:44 +0900

    package재설치

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -10,13 +10,20 @@ export default function SearchBar() {
   const dispatch = useDispatch();
 
   const [open, setOpen] = useState(false);
-
   const [value, setValue] = useState("");
+  const [originPath, setOriginPath] = useState(null);
 
   const barRef = useRef(null);
 
   const handleButtonClick = () => {
-    setOpen((prev) => !prev);
+    setOpen((prev) => {
+      const nextOpen = !prev;
+
+      if (nextOpen && !location.pathname.startsWith("/search")) {
+        setOriginPath(location.pathname);
+      }
+      return nextOpen;
+    });
   };
   const handleChange = (e) => {
     setValue(e.target.value);
@@ -45,9 +52,16 @@ export default function SearchBar() {
     const id = setTimeout(() => {
       if (!searchValue) {
         dispatch(searchSlice.actions.resetSearch());
+
+        if (location.pathname.startsWith("/search") && originPath) {
+          navigate(originPath);
+        }
         return;
       }
       if (!location.pathname.startsWith("/search")) {
+        if (!originPath) {
+          setOriginPath(location.pathname);
+        }
         navigate("/search");
       }
       dispatch(searchSlice.actions.resetSearch());
@@ -55,11 +69,36 @@ export default function SearchBar() {
       dispatch(fetchSearchPage());
     }, 400);
     return () => clearTimeout(id);
-  }, [value, open, location.pathname, navigate, dispatch]);
+  }, [value, open, location.pathname, originPath, navigate, dispatch]);
 
   return (
-    <div ref={barRef}>
-      <button onClick={handleButtonClick}>🔍</button>
+    <div ref={barRef} className="relative flex items-center gap-2">
+      <button
+        onClick={handleButtonClick}
+        className="
+      p-2
+      rounded-full
+      transition
+      duration-200
+      hover:bg-white/20
+      cursor-pointer
+      flex items-center justify-center
+    "
+      >
+        <svg
+          className="w-5 h-5 text-white"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          viewBox="0 0 24 24"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M21 21l-4.35-4.35M11 18a7 7 0 100-14 7 7 0 000 14z"
+          />
+        </svg>
+      </button>
       {open && (
         <input
           type="text"
