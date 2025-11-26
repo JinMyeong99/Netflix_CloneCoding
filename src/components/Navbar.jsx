83f1608c5dca438f9651bd9fbfb23f06d8f0ecc6
JinMyeong Choi <ymo6538@gmail.com>
2025-11-26 16:08:09 +0900

    navbar 스타일 수정

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,51 @@
 import { Link } from "react-router-dom";
 import Netflix_logo from "../assets/Netflix_logo.png";
 import SearchBar from "./Searchbar";
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50);
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <header className="flex items-center justify-between h-16 bg-black text-white">
-      <div className="flex items-center justify-between ">
-        <Link to="/" className="mr-10">
-          <img className="flex h-6" src={Netflix_logo} />
-        </Link>
-        <nav className="flex gap-5">
-          <Link className="hover:text-gray-300" to="/">
-            홈
+    <header
+      className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${isScrolled ? "bg-black" : "bg-transparent"}`}
+    >
+      <div className="mx-auto flex h-16 max-w-7xl items-center justify-between px-4 ">
+        <div className="flex items-center gap-8">
+          <Link to="/" className="flex items-center">
+            <img className="h-6 w-auto" src={Netflix_logo} />
           </Link>
-          <Link className="hover:text-gray-300" to="/series">
-            시리즈
-          </Link>
-          <Link className="hover:text-gray-300" to="/movie">
-            영화
-          </Link>
-          <Link className="hover:text-gray-300" to="/trending">
-            트렌드
-          </Link>
-          <Link className="hover:text-gray-300" to="/favorite">
-            내가 찜한 리스트
-          </Link>
-        </nav>
-      </div>
-      <div className="flex items-center">
-        <SearchBar />
-        <div>로그인</div>
+          <nav className="flex gap-5">
+            <Link className="hover:text-gray-300" to="/">
+              홈
+            </Link>
+            <Link className="hover:text-gray-300" to="/series">
+              시리즈
+            </Link>
+            <Link className="hover:text-gray-300" to="/movie">
+              영화
+            </Link>
+            <Link className="hover:text-gray-300" to="/trending">
+              트렌드
+            </Link>
+            <Link className="hover:text-gray-300" to="/favorite">
+              내가 찜한 리스트
+            </Link>
+          </nav>
+        </div>
+        <div className="flex items-center gap-4">
+          <SearchBar />
+          <div>로그인</div>
+        </div>
       </div>
     </header>
   );
