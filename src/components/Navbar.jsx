cb7cb4ad41da2341f629ee4a8362e4d95925aa77
JinMyeong Choi <ymo6538@gmail.com>
2025-11-26 20:53:44 +0900

    Navbar padding 수정

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,10 +19,10 @@ export default function Navbar() {
     <header
       className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${isScrolled ? "bg-black" : "bg-transparent"}`}
     >
-      <div className="mx-auto flex h-16 max-w-7xl items-center justify-between px-4 ">
+      <div className="flex h-16 items-center justify-between px-[4%]">
         <div className="flex items-center gap-8">
-          <Link to="/" className="flex items-center">
-            <img className="h-6 w-auto" src={Netflix_logo} />
+          <Link to="/">
+            <img className="h-6" src={Netflix_logo} />
           </Link>
           <nav className="flex gap-5">
             <Link className="hover:text-gray-300" to="/">
