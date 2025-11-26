8042be770e8f2b68a844d54bfe4fa973d80502c6
JinMyeong Choi <ymo6538@gmail.com>
2025-11-26 13:16:50 +0900

    무비 카드 수정

--- src/components/Moviecard.jsx
+++ src/components/Moviecard.jsx
@@ -1,18 +1,18 @@
 import { ImageUrl } from "../api/tmdb";
 
 export default function MovieCard({ movie }) {
-  const title = movie.title || movie.name;
+  // const title = movie.title || movie.name;
   const poster = ImageUrl(movie.poster_path || movie.backdrop_path, "w200");
-  const year =
-    movie.release_date?.slice(0, 4) || movie.first_air_date?.slice(0, 4) || "";
+  // const year =
+  //   movie.release_date?.slice(0, 4) || movie.first_air_date?.slice(0, 4) || "";
 
   return (
     <article>
       <div>{poster ? <img src={poster} /> : <div>NO IMAGE</div>}</div>
-      <div>
+      {/* <div>
         <div>{title}</div>
         {year && <div>{year}</div>}
-      </div>
+      </div> */}
     </article>
   );
 }
