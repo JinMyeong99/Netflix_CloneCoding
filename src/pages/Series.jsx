fbc84fc6cf9a49f3bfbcfeddfbe4d8f8cf1cadfa
JinMyeong Choi <ymo6538@gmail.com>
2025-11-28 17:59:57 +0900

    시리즈페이지 장르 선택자 추가

--- src/pages/Series.jsx
+++ src/pages/Series.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { seriesSlice } from "../RTK/series/seriesSlice";
 import { fetchSeriesPage } from "../RTK/series/seriesThunk";
@@ -10,6 +10,9 @@ export default function Series() {
   const { list, loading, hasMore, page, error } = useSelector(
     (state) => state.series
   );
+
+  const { seriesGenres } = useSelector((state) => state.genre);
+  const [selectedGenreId, setSelectedGenreId] = useState("");
 
   useEffect(() => {
     if (page === 0 && list.length === 0) {
@@ -29,12 +32,33 @@ export default function Series() {
     hasMore,
     onLoadMore: loadMore,
   });
+
+  const filteredSeries = useMemo(() => {
+    if (!selectedGenreId) return list;
+    const genreId = Number(selectedGenreId);
+
+    return list.filter((series) => series.genre_ids.includes(genreId));
+  }, [list, selectedGenreId]);
+
   return (
     <div className="mx-auto max-w-[90%] pb-25">
       <h2 className="text-4xl font-bold my-4">인기 시리즈</h2>
+      <div>
+        <select
+          value={selectedGenreId}
+          onChange={(e) => setSelectedGenreId(e.target.value)}
+        >
+          <option value="">장르 전체</option>
+          {seriesGenres.map((genre) => (
+            <option key={genre.id} value={genre.id}>
+              {genre.name}
+            </option>
+          ))}
+        </select>
+      </div>
       {error && <div>{error}</div>}
       <div className="flex flex-wrap justify-between gap-y-30">
-        {list.map((series) => (
+        {filteredSeries.map((series) => (
           <MovieCard key={series.id} movie={series} />
         ))}
       </div>
