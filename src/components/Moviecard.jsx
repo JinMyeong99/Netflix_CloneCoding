f167dfc8f39d18dac0cc48ebc150ee2fe2feacbf
JinMyeong Choi <ymo6538@gmail.com>
2025-11-28 11:50:15 +0900

    주석 삭제

--- src/components/Moviecard.jsx
+++ src/components/Moviecard.jsx
@@ -1,10 +1,7 @@
 import { ImageUrl } from "../api/tmdb";
 
 export default function MovieCard({ movie }) {
-  // const title = movie.title || movie.name;
   const poster = ImageUrl(movie.poster_path || movie.backdrop_path, "w400");
-  // const year =
-  //   movie.release_date?.slice(0, 4) || movie.first_air_date?.slice(0, 4) || "";
 
   return (
     <article>
@@ -15,10 +12,6 @@ export default function MovieCard({ movie }) {
           <div>NO IMAGE</div>
         )}
       </div>
-      {/* <div>
-        <div>{title}</div>
-        {year && <div>{year}</div>}
-      </div> */}
     </article>
   );
 }
