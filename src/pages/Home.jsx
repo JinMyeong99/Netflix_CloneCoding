34ca31a6327cdc46a2403b25b6ef4f63efc9266f
JinMyeong Choi <ymo6538@gmail.com>
2025-12-21 16:10:56 +0900

    fix: 변수명 변경

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,13 +22,13 @@ export default function Home() {
     fetchHomeData();
   }, [fetchHomeData]);
 
-  const popularWithGenre = useGenreName(popular, "movie");
-  const topRatedWithGenre = useGenreName(topRated, "movie");
-  const actionAdventureWithGenre = useGenreName(actionAdventure, "movie");
-  const comedyMoviesWithGenre = useGenreName(comedyMovies, "movie");
-  const sciFiFantasyWithGenre = useGenreName(sciFiFantasy, "movie");
+  const popularMovie = useGenreName(popular, "movie");
+  const topRatedMovie = useGenreName(topRated, "movie");
+  const actionAdventureMovie = useGenreName(actionAdventure, "movie");
+  const comedyMovie = useGenreName(comedyMovies, "movie");
+  const sciFiFantasyMovie = useGenreName(sciFiFantasy, "movie");
 
-  const heroContent = popularWithGenre[0];
+  const heroContent = popularMovie[0];
 
   const {
     selectedContent,
@@ -65,35 +65,35 @@ export default function Home() {
       <div className="pb-10 px-[5%]">
         <SectionRow
           title="지금 가장 인기 있는 영화"
-          content={popularWithGenre}
+          content={popularMovie}
           openDetail={openDetail}
           toggleFavorite={toggleFavorite}
           openTrailer={openTrailer}
         />
         <SectionRow
           title="최고 평점 영화"
-          content={topRatedWithGenre}
+          content={topRatedMovie}
           openDetail={openDetail}
           toggleFavorite={toggleFavorite}
           openTrailer={openTrailer}
         />
         <SectionRow
           title="액션 ∙ 모험 인기 영화"
-          content={actionAdventureWithGenre}
+          content={actionAdventureMovie}
           openDetail={openDetail}
           toggleFavorite={toggleFavorite}
           openTrailer={openTrailer}
         />
         <SectionRow
           title="코미디 TOP 콘텐츠"
-          content={comedyMoviesWithGenre}
+          content={comedyMovie}
           openDetail={openDetail}
           toggleFavorite={toggleFavorite}
           openTrailer={openTrailer}
         />
         <SectionRow
           title="SF ∙ 판타지 추천"
-          content={sciFiFantasyWithGenre}
+          content={sciFiFantasyMovie}
           openDetail={openDetail}
           toggleFavorite={toggleFavorite}
           openTrailer={openTrailer}
