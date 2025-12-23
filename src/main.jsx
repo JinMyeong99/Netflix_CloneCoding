4ce917162c2550e84da8ef6c885c05ef9f933107
JinMyeong Choi <ymo6538@gmail.com>
2025-12-23 14:55:00 +0900

    feat: QueryClient 전역 설정

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,24 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 3,
+      gcTime: 1000 * 60 * 15,
+      retry: 1,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")).render(
-
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </QueryClientProvider>
 );
