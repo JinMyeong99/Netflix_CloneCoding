e8d1ba215f82888dc6d4771527bdeae1f4bd6313
JinMyeong Choi <ymo6538@gmail.com>
2026-01-08 00:53:46 +0900

    pref: preload CSS를 stylesheet로 전환 콘솔 경고 지우기

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.jsx";
 
 const queryClient = new QueryClient({
